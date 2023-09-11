d12e9a0991c6239e259a4de2c1219b40fc8e294d
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-09-11 13:48:45 -0400

    controller and routes working

--- backend/routes/atlastRoutes.js
+++ backend/routes/atlastRoutes.js
@@ -24,14 +24,20 @@ const { getWorkout,
 //     res.status(200).json({ message:`Delete ${req.params.id}` })  
 // })
 
-// Making the Routes Dynamic and clean
-// GET = R(Read) 
-router.get('/', getWorkout) //replaced entire funtion from original route with atlastGET (functionality of the function is now in the controller file atlastContoller.js)
-// POST = C(Create)
-router.post('/', setWorkout)
-// PUT = U(Update)
-router.put('/:id', updateWorkout)
-// Delete = D(Delete)
-router.delete('/:id', deleteWorkout)
+// // Making the Routes Dynamic and clean
+// // GET = R(Read) 
+// router.get('/', getWorkout) //replaced entire funtion from original route with atlastGET (functionality of the function is now in the controller file atlastContoller.js)
+// // POST = C(Create)
+// router.post('/', setWorkout)
+// // PUT = U(Update)
+// router.put('/:id', updateWorkout)
+// // Delete = D(Delete)
+// router.delete('/:id', deleteWorkout)
+
+//making the routes even cleaner by chaining the shared routes from above ('/') && ('/:id')
+//this is get and post routes in one line
+router.route('/').get(getWorkout).post(setWorkout)
+//this is update and delete routes on one line (don't forget the :id)
+router.route('/:id').put(updateWorkout).delete(deleteWorkout)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,14 @@ const port = process.env.PORT || 3000 //variable holding the port number to list
 
 const app = express() //makes the instance of the express server that was imported on line 1
 
+//  MIDDLEWARE
+
+
+//Middleware forcing the routes to include /atlastRB first before the rest of the route
 app.use('/atlastRB', require('./routes/atlastRoutes')) //makes the route atlastRB
                                                        //& require is like an import so it's looking in the routes folder and then that file (atlastRoutes)
 
+
+//PORT 3000
 app.listen(port, () => console.log       
 (`listening to server on port ${port}`)) //tells the app to 'listen' to port which is the variable assigned 3000, then the string tells you what port it's listening for as well by using jsx injection for port
