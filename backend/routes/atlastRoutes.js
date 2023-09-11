d5eff34d97ab3a0c4b00498354aa46bff06635ff
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-09-11 11:25:53 -0400

    CRUD ROUTES Working

--- backend/routes/atlastRoutes.js
+++ backend/routes/atlastRoutes.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+//ROUTES (Starting with CRUD - Create Read Update Delete)
+// GET = R(Read) 
 router.get('/', (req, res) => {  // you only need the ('/') bc the route was already specified inn servers app.use //working test router(returns the welcome string)
-    res.status(200).json({ message:'welcome to home page'})  //changed to json to allow jsx injection of key value pair
+    res.status(200).json({ message:'See Workout'})  //changed to json to allow jsx injection of key value pair ({message: 'value"})
+})
+// POST = C(Create)
+router.post('/', (req, res) => {  
+    res.status(200).json({ message:'Create a Workout'})  
+})
+// PUT = U(Update)
+router.put('/:id', (req, res) => {  
+    res.status(200).json({ message:`Update ${req.params.id}` })  //request params is requesting the parameter of id put into router as :id to make it whatever value you change it to
+})
+// Delete = D(Delete)
+router.delete('/:id', (req, res) => {  
+    res.status(200).json({ message:`Delete ${req.params.id}` })  
 })
 
 module.exports = router
