03b87d5821a12515fffbc94d0546e2ca5c2030ef
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-09-11 14:41:37 -0400

    controller async and handler setup

--- backend/controllers/atlastController.js
+++ backend/controllers/atlastController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler') //wrapping all your asyncs w/ the function in this file in this imported handler 
+
 // @desc    Get workouts
 // @route   GET /atlastRB/
 // @access  Private
-const getWorkout = (req, res) => {
+const getWorkout = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'See Workouts' }) 
-}
+})
 
 // @desc    Set workout
 // @route   POST /atlastRB/
 // @access  Private
-const setWorkout = (req, res) => {  
+const setWorkout = asyncHandler(async (req, res) => {  
     if(!req.body.text) {  //checking the body data in postman (this is made usable using middleware in server.js)
         res.status(400)
         throw new Error('Please add a text field') //to show error (but shows in html)
     } 
 
     res.status(200).json({ message:'Create a Workout'}) 
-}
+})
 
 // @desc    Update workout
 // @route   PUT /atlastRB/:id
 // @access  Private
-const updateWorkout = (req, res) => {
+const updateWorkout = asyncHandler(async (req, res) => {
     res.status(200).json({ message:`Update ${req.params.id}` })
-}
+})
 
 // @desc    Delete workout
 // @route   DELETE /atlastRB/:id
 // @access  Private
-const deleteWorkout = (req, res) => {
+const deleteWorkout = asyncHandler(async (req, res) => {
     res.status(200).json({ message:`Delete ${req.params.id}` }) 
-}
+})
 
 
 //exporting all functions put into variables above inside {} below
