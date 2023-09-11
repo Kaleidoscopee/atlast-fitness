6d0d1d6bfdedb64f276a38eff066a6c338b5654a
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-09-11 12:42:22 -0400

    route functions put into controller

--- backend/controllers/atlastController.js
+++ backend/controllers/atlastController.js
@@ -1,9 +1,36 @@
-const atlastGET = (req, res) => {
-    res.status(200).json({ message: 'See Workout' })
+// @desc    Get workouts
+// @route   GET /atlastRB/
+// @access  Private
+const getWorkout = (req, res) => {
+    res.status(200).json({ message: 'See Workouts' }) 
+}
+
+// @desc    Set workout
+// @route   POST /atlastRB/
+// @access  Private
+const setWorkout = (req, res) => {
+    res.status(200).json({ message:'Create a Workout'}) 
+}
+
+// @desc    Update workout
+// @route   PUT /atlastRB/:id
+// @access  Private
+const updateWorkout = (req, res) => {
+    res.status(200).json({ message:`Update ${req.params.id}` })
+}
+
+// @desc    Delete workout
+// @route   DELETE /atlastRB/:id
+// @access  Private
+const deleteWorkout = (req, res) => {
+    res.status(200).json({ message:`Delete ${req.params.id}` }) 
 }
 
 
 //exporting atlastGET above
 module.exports = {    
-    atlastGET,
+    getWorkout,
+    setWorkout,
+    updateWorkout,
+    deleteWorkout,
 }
--- backend/routes/atlastRoutes.js
+++ backend/routes/atlastRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()  //typical express setup
-const { atlastGET } = require('../controllers/atlastController') //inside the const is the atlastGET function from atlastControllers //require - so basically importing atlastController file from controllers folder
+const { getWorkout,
+        setWorkout,
+        updateWorkout,
+        deleteWorkout, 
+} = require('../controllers/atlastController') //inside the const is the atlastGET function from atlastControllers //require - so basically importing atlastController file from controllers folder
 
 //ROUTES (Starting with CRUD - Create Read Update Delete)
 // // GET = R(Read) 
@@ -22,18 +26,12 @@ const { atlastGET } = require('../controllers/atlastController') //inside the co
 
 // Making the Routes Dynamic and clean
 // GET = R(Read) 
-router.get('/', atlastGET) //replaced entire funtion from original route with atlastGET (functionality of the function is now in the controller file atlastContoller.js)
+router.get('/', getWorkout) //replaced entire funtion from original route with atlastGET (functionality of the function is now in the controller file atlastContoller.js)
 // POST = C(Create)
-router.post('/', (req, res) => {  
-    res.status(200).json({ message:'Create a Workout'})  
-})
+router.post('/', setWorkout)
 // PUT = U(Update)
-router.put('/:id', (req, res) => {  
-    res.status(200).json({ message:`Update ${req.params.id}` })  //request params is requesting the parameter of id put into router as :id to make it whatever value you change it to
-})
+router.put('/:id', updateWorkout)
 // Delete = D(Delete)
-router.delete('/:id', (req, res) => {  
-    res.status(200).json({ message:`Delete ${req.params.id}` })  
-})
+router.delete('/:id', deleteWorkout)
 
 module.exports = router
