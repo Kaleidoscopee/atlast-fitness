0e2e04acc92766f4ff09d69664a91a1ec3715b24
Kaleidoscopee <133397937+Kaleidoscopee@users.noreply.github.com>
2023-09-12 15:50:03 -0400

    usercontroller and user routes working

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser } = require('../controllers/userController.js')
+const { 
+    registerUser,
+    loginUser,
+    getMe,
+} = require('../controllers/userController.js')  
 
-router.post('/', registerUser)
+router.post('/', registerUser)  //post is to add so here we are adding a user (POST, create, etc) // the registerUser variable being used is above and holds the userController files function for that route action
+router.post('/login', loginUser)
+router.get('/me', getMe)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ app.use(express.json()) //changes JSON coming in from the server into usable Jav
 app.use(express.urlencoded({ extended: false })) //similar to above but for urlEncoded
 
 //Middleware forcing the routes to include /atlastRB first before the rest of the route
-app.use('/atlast/workoutbuilder', require('./routes/atlastRoutes')) //makes the route atlastRB //& require is like an import so it's looking in the routes folder and then that file (atlastRoutes)
-app.use('/atlast/users', require('./routes/atlastRoutes'))
+app.use('/atlast/RB', require('./routes/atlastRoutes')) //makes the route atlast/RB/ //& require is like an import so it's looking in the routes folder and then that file (atlastRoutes)
+app.use('/atlast/users', require('./routes/userRoutes')) //makes the route name atlast/users and looks for the route types in userRoutes.js in the routes folder
 
 app.use(errorHandler) //use function in errorMiddleware file
 
